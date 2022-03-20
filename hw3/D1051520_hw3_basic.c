47e0ec6960e1a54deed3917d5a9587dfbbe69228
gene323 <geneho0323@gmail.com>
2022-03-21 01:23:34 +0800

    first commit

--- hw3/D1051520_hw3_basic.c
+++ hw3/D1051520_hw3_basic.c
@@ -1,35 +1,83 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+typedef struct Vocabulary{
+    bool flag;
+    char voc[10];
+    struct Vocabulary *nextVoc;
+}Vocabulary;
+
+FILE *history;
+FILE *words;
+
+void isFileOpen(char *path);
+int countWord(FILE *fp);
 
 int main(){
+
+    isFileOpen("history.txt");
+    isFileOpen("words.txt");
 
     FILE *history = fopen("history.txt", "r");
     FILE *words = fopen("words.txt", "r");
 
-    if( (history = fopen("history.txt", "r")) == NULL){
-        puts("The history file can not open");
+    Vocabulary *headVoc, *curVoc, *tailVoc;
+    const int COUNT = countWord(words);
+    char str[10], targetStr[10];
+
+    for(int i=0; i<COUNT; i++){
+        curVoc = (Vocabulary*) malloc(sizeof(Vocabulary));
+        fscanf(words, "%s", str);
+
+        fseek(history, 17, SEEK_SET);
+        while(fscanf(history, "%*s %*s %s", targetStr) != EOF){
+            for(int i=0; i<strlen(targetStr); i++){
+                targetStr[i] = tolower(targetStr[i]);
+            }
+            if(strcmp(targetStr, str) == 0){
+                curVoc->flag = 1;
+                break;
+            }
+            else{
+                curVoc->flag = 0;
+            }
+        }
+        strcpy(curVoc->voc, str);
+        if(i == 0){
+            headVoc = curVoc;
+        }
+        else
+            tailVoc->nextVoc = curVoc;
+        curVoc->nextVoc = NULL;
+        tailVoc = curVoc;
     }
-    else{
-        puts("The history file open successfully");
+    curVoc = headVoc;
+    while(curVoc != NULL){
+        printf("%d %s\n", curVoc->flag, curVoc->voc);
+        curVoc = curVoc->nextVoc;
     }
 
-    if( (words = fopen("words.txt", "r")) == NULL){
-        puts("The words file can not open");
-    }
-    else{
-        puts("The words file open successfully");
-    }
-
-    char date[20];
-    char day[20];
-    char answer[20];
-
-    fseek(history, 17, SEEK_SET);
-    fscanf(history, "%*s %*s %s", answer);
-
-    while(!feof(history)){
-        printf("%s\n", answer);
-        fscanf(history, "%*s %*s %s", answer);
-    }
     return 0;
-}
+}//end main func
+
+void isFileOpen(char *path){
+    if( fopen(path, "r") == NULL){
+        printf("The %s file can not open", path);
+    }
+    else{
+        printf("The %s file open successfully", path);
+    }
+    puts("");
+}//end isFileOpen func
+
+int countWord(FILE *fp){
+    int count = 0;
+    while(fscanf(fp, "%*s") != EOF){
+        ++count;
+    }
+    fseek(fp, 0, SEEK_SET);
+    return count;
+}//end countWord func
