21a07ad7790c7d1edfdcb447fe6a7fde2bce25e7
Gene Ho <geneho0323@gmail.com>
2022-12-05 15:09:30 +0800

    add vending machine product and operate

--- OOP/H3/src/Main.java
+++ OOP/H3/src/Main.java
@@ -1,11 +1,60 @@
-package VendingMachine;
-import VendingMachine.util.VendingMachine;
+import util.VendingMachine;
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         VendingMachine vendingMachine = new VendingMachine();
-        vendingMachine.select(VendingMachine.EVIAN_MINERAL_WATER);
-        vendingMachine.show();
-        System.out.print(VendingMachine.COCA_COLA.getPrice());
+
+        char op = '1';
+        while(op != 'e'){
+            System.out.print("a. Deposit bill(s) or coin(s),\n"
+                            + "b. Select product(s),\n"
+                            + "c. Cancel a product,\n"
+                            + "d. Purchase product(s),\n"
+                            + "e. Abort transaction.\n"
+                            + "> "
+            );
+            op = sc.next().charAt(0);
+
+            if(op == 'a'){
+                char moneyType;
+                do{
+                    System.out.print("enter B for bill, C for coin, other letter to quti\n> ");
+                    moneyType = sc.next().charAt(0);
+
+                    if(moneyType == 'B'){
+                        while(true){
+                            System.out.print("enter 1, 5, 10, 20 dollars, 0 to quit\n> ");
+                            double money = sc.nextDouble();
+                            if(money == 1 || money == 5 || money == 10 || money == 20){
+                                vendingMachine.depositMoney(money);
+                            }
+                            else if(money == 0)
+                                break;
+                            else{
+                                System.out.println("wrong value");
+                            }
+                        }
+                    }
+                    else if(moneyType == 'C'){
+                        while(true){
+                            System.out.print("enter 5, 10, 25 cents, 0 to quit\n> ");
+                            double money = sc.nextDouble();
+                            if(money == 5 || money == 10 || money == 25){
+                                vendingMachine.depositMoney(money);
+                            }
+                            else if(money == 0)
+                                break;
+                            else{
+                                System.out.println("wrong value");
+                            }
+                        }
+                    }
+                } while(moneyType == 'B' || moneyType == 'C');
+            }
+        }
+
+        sc.close();
     }
 }
--- OOP/H3/src/util/VendingMachine.java
+++ OOP/H3/src/util/VendingMachine.java
@@ -1,14 +1,20 @@
-package VendingMachine.util;
+package util;
 import java.util.ArrayList;
 
 public class VendingMachine{
     protected ArrayList<Product> shoppingCart;
+    private double money;
     public final static Product COCA_COLA = new Product("Coca Cola", 1.65);
     public final static Product ORANGE_JUICE = new Product("Minute Maid Orange Juice", 3.50);
     public final static Product EVIAN_MINERAL_WATER = new Product("Evian Mineral Water", 2.80);
-
+    public final static Product MM_CHOCOLATE = new Product("M&M's Chocolate", 1.5); 
+    public final static Product HERSHEY_CHOCOLATE_BAR = new Product("Hershey's Chocolate Bar", 1.85);
+    public final static Product OREO_COOKIE = new Product("Oreo Cookies", 1.0);
+    public final static Product DORITOS_TORTILLA_CHIP = new Product("Doritos Tortilla Chips", 3.25);
+    public final static Product PRINGLES_POTATO_CHIP = new Product("Pringles Potato Chips", 3.4);
     public VendingMachine(){
-        shoppingCart = new ArrayList<Product>();
+        this.shoppingCart = new ArrayList<Product>();
+        this.money = 0;
     }
 
     public void select(Product product){
@@ -16,20 +22,27 @@ public class VendingMachine{
     }
     public void show(){
         for(Product it: shoppingCart){
-            System.out.println(it.getProdcutName());
+            System.out.println(it.getName());
         }
+    }
+    public void depositMoney(double money){
+        this.money += money;
+
+    }
+    public double getMoney(){
+        return this.money;
     }
 
     public static class Product{
-        protected String productName;
+        protected String name;
         protected double price;
         public Product(String name, double price){
-            this.productName = name;
+            this.name = name;
             this.price = price;
         }
 
-        public String getProdcutName(){
-            return productName;
+        public String getName(){
+            return name;
         }
         public double getPrice(){
             return price;
