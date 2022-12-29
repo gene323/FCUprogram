7aad4e8326ced6c6bda2719a01d815f3bb00ae1a
Gene Ho <geneho0323@gmail.com>
2022-12-29 23:10:14 +0800

    complete H5

--- OOP/H5/src/Main.java
+++ OOP/H5/src/Main.java
@@ -4,13 +4,29 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
-public class Main{
+public class Main implements ActionListener{
+    private static final int TIME_LEAVE = 2000;
     private JFrame frame;
-    private ArrayList<JLabel> product;
+    private ArrayList<Product> product;
+    private JLabel moneyLabel;
     private double money;
+    private JButton btn1;
+    private JButton btn2;
+    private JButton btn3;
+    private JButton btn4;
+
     public Main(){
         frame = new JFrame("Vending Machine");
-        product = new ArrayList<JLabel>();
+        product = new ArrayList<Product>();
+        moneyLabel = new JLabel("Money: 0", SwingConstants.CENTER);
+        btn1 = new JButton("1");
+        btn2 = new JButton("5");
+        btn3 = new JButton("10");
+        btn4 = new JButton("15");
+        btn1.addActionListener(this);
+        btn2.addActionListener(this);
+        btn3.addActionListener(this);
+        btn4.addActionListener(this);
         money = 0;
         frame.setSize(1000, 1000);
         frame.setLayout(new GridBagLayout());
@@ -24,6 +40,46 @@ public class Main{
         product.add(new Product("Oreo Cookies", "../img/oreo.jpg", 1, 6, 6));
         product.add(new Product("Doritos Tortilla Chips", "../img/doritos.jpg", 3.25, 12, 0));
         product.add(new Product("Pringles Potato Chips", "../img/pringles.jpg", 3.4, 12, 3));
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(10, 10, 10, 10);
+        c.gridx = GridBagConstraints.RELATIVE;
+        c.gridy = 18;
+        c.gridwidth = 3;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        frame.add(btn1, c);
+        frame.add(btn2, c);
+        
+        c.gridy = 19;
+        frame.add(btn3, c);
+        frame.add(btn4, c);
+
+        c.gridwidth = 3;
+        c.gridheight = 2;
+        c.gridy = 18;
+        c.fill = GridBagConstraints.BOTH;
+        moneyLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        frame.add(moneyLabel, c);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == btn1){
+            money += 1;
+        }
+        else if(e.getSource() == btn2){
+            money += 5;
+        }
+        else if(e.getSource() == btn3){
+            money += 10;
+        }
+        else if(e.getSource() == btn4){
+            money += 15;
+        }
+        moneyLabel.setText("Money: " + money);
+        product.forEach((p) -> {
+            p.enableBuy();
+        });
     }
 
     public static void main(String[] args){
@@ -31,43 +87,42 @@ public class Main{
         gui.frame.setVisible(true);
     }
 
-    public class Product extends JLabel implements ActionListener{
+    public class Product implements ActionListener{
         private JLabel name;
         private JLabel image;
-        private JLabel quantity;
-        private JLabel price;
+        private JLabel priceLabel;
         private JButton select;
-        private JButton cancel;
-        private int number;
+        private double priceNum;
         private int row;
         private int col;
 
         public Product(String name, String image, double price, int row, int col){
             this.name = new JLabel(name);
             this.image = new JLabel(new ImageIcon(image));
-            this.select = new JButton("+");
-            this.cancel = new JButton("-");
-            this.quantity = new JLabel("0");
-            this.number = 0;
-            this.price = new JLabel("Price: " + price, SwingConstants.CENTER);
+            this.select = new JButton("buy");
+            this.priceLabel = new JLabel("Price: " + price, SwingConstants.CENTER);
+            this.priceNum = price;
             this.row = row;
             this.col = col;
             this.select.addActionListener(this);
-            this.cancel.addActionListener(this);
+            this.select.setEnabled(false);
             setGrid(row, col);
         }
 
         public void actionPerformed(ActionEvent e){
-            if(e.getSource() == this.select){
-                this.number++;
-                this.quantity.setText(Integer.toString(number));
-            }
-            else if(e.getSource() == this.cancel){
-                if(number > 0){
-                    this.number--;
-                    this.quantity.setText(Integer.toString(number));
-                }
-            }
+            money -= priceNum;
+            moneyLabel.setText("Money: " + money);
+            if(money < this.priceNum)
+                this.select.setEnabled(false);
+            product.forEach((p) -> {
+                if(p.priceNum > money)
+                    p.select.setEnabled(false);
+            });
+        }
+
+        public void enableBuy(){
+            if(money >= priceNum)
+                this.select.setEnabled(true);
         }
 
         public void setGrid(int row, int col){
@@ -87,20 +142,9 @@ public class Main{
             frame.add(image, c);
 
             c.gridx = col;
-            c.gridwidth = 3;
             c.gridheight = 1;
             c.fill = GridBagConstraints.HORIZONTAL;
-            frame.add(price, c);
-
-            c.gridwidth = 1;
-            c.gridheight = 1;
-            c.fill = GridBagConstraints.NONE;
-            frame.add(cancel, c);
-
-            c.gridx = col + 1;
-            frame.add(quantity, c);
-
-            c.gridx = col + 2;
+            frame.add(priceLabel, c);
             frame.add(select, c);
         }
     }
